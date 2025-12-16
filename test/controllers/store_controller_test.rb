d47b7aac269bf7637ea259bc424e9beb2bea52c1
sanketsute <jsanketsute@gmail.com>
2025-12-16 23:51:59 +0530

    ain page changes

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -4,5 +4,7 @@ class StoreControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get store_index_url
     assert_response :success
+    assert_select "h1", "The Pragmatic Collection"
+    assert_select "li a", minimum: 7
   end
 end
